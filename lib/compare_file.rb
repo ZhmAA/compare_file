026874e41bce94825ba4de1384d9b9d3aa3d84e1
ZhmAA <azhm@ukr.net>
2016-11-17 01:12:17 +0200

    create simple gem app

---
+++ lib/compare_file.rb
@@ -0,0 +1,71 @@
+require "compare_file/version"
+
+module CompareFile
+  class Compare
+
+    def initialize(file, sec_file)
+      @file  = file
+      @sec_file = sec_file
+    end
+
+    def load(file_path)
+      unless file_path.empty?
+        return File.readlines(file_path).each { |line| line.delete!("\n") }
+      else
+        raise 'File not found! Try again please.'
+      end
+    end
+
+    def comparefiles
+      first  = load(@file)
+      second = load(@sec_file)
+      lines = []
+      different = first | second
+      same = first & second
+      different.each_with_index do | val, i |
+        if (same.include? first[i])
+          val1 = first[i]
+          val2 = first[i]
+        else
+          val1 = (same.include? first[i])  ? nil : first[i]
+          val2 = (same.include? second[i]) ? nil : second[i]
+        end
+        lines << [val1, val2] unless val1.nil? && val2.nil?
+      end
+      conclusion(result(lines))
+    end
+
+    private
+
+    def result(lines)
+      line  = []
+      lines.each_with_index do | val, i |
+        line << i + 1
+        if val[0] == val[1]
+          line << ' '
+          line << val[0]
+        elsif val[0].nil?
+          line << '+'
+          line << val[1]
+        elsif val[1].nil?
+          line << '-'
+          line << val[0]
+        elsif val[0] != val[1]
+          line << '*'
+          line << "#{ val[0] } | #{ val[1] }"
+        end
+      end
+      return line
+    end
+
+    def conclusion(res)
+      res.each_with_index do | val, i |
+        if ((i + 1) % 3 == 0) 
+          print "#{ val }\n" 
+        else
+          print "#{ val } "
+        end
+      end
+    end
+  end
+end
---
+++ lib/compare_file/version.rb
@@ -0,0 +1,3 @@
+module CompareFile
+  VERSION = "0.1.0"
+end
